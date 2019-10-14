66c757a5daaa79d861f985f505f8766bade0478b
Jeff Lewis <jeffylew77@gmail.com>
2019-10-13 18:16:53 -0700

    [Fix] Linting

--- redux/reducers/counterReducer.js
+++ redux/reducers/counterReducer.js
@@ -7,26 +7,25 @@ const initialState = {
     firebaseError: null,
   };
   
-  // Document Reducer
-  export default function documentReducer (state = initialState, action) {
-    switch (action.type) {
-      // Increase Counter
-      case 'INCREASE_COUNTER':
-        return {
-          ...state,
-          loading: true,
-        }
-  
-      // Decrease Counter
-      case 'DECREASE_COUNTER':
-        return {
-          ...state,
-          loading: false,
-        }
-  
+// Document Reducer
+export default function documentReducer (state = initialState, action) {
+  switch (action.type) {
+    // Increase Counter
+    case 'INCREASE_COUNTER':
+      return {
+        ...state,
+        loading: true,
+      }
 
-      // Default
-      default:
-        return state;
-    }
+    // Decrease Counter
+    case 'DECREASE_COUNTER':
+      return {
+        ...state,
+        loading: false,
+      }
+
+    // Default
+    default:
+      return state;
   }
+}
