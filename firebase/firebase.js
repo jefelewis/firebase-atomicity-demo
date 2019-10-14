04ebb9aff3e2853e4ca708c34e8a664d3bb50c89
Jeff Lewis <jeffylew77@gmail.com>
2019-10-13 19:01:32 -0700

    [Dev] Adding Firebase config

---
+++ firebase/firebase.js
@@ -0,0 +1,26 @@
+// Imports: Dependencies
+import firebase from 'firebase';
+import '@firebase/firestore';
+import ReduxSagaFirebase from 'redux-saga-firebase';
+
+// Imports: Firebase Config
+import firebaseConfig from '../config/config.js';
+
+// Firebase: Initialize
+const firebaseApp = firebase.initializeApp({
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  databaseURL: firebaseConfig.databaseURL,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+});
+
+// Redux Saga Firebase: Initialize
+const reduxSagaFirebase = new ReduxSagaFirebase(firebaseApp);
+
+// Firebase: Counter Reference
+export const counterRef = firebase.firestore().collection('counter/counter');
+
+// Exports
+export default reduxSagaFirebase;
